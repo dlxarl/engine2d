f9664d152ae41910c2961bc4444f8400d3024349
ReddiKit <zinchukillia@gmail.com>
2025-10-31 09:33:07 +0100

    Fixed scenes

--- src/scenes/Scene2.cpp
+++ src/scenes/Scene2.cpp
@@ -1,38 +1,64 @@
-#include "headers/Scene2.h"
 #include "headers/Scene1.h"
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
+#include "headers/Scene2.h"
+
+Scene2::Scene2()
+    : line(Point2D(100, 100), Point2D(700, 100), Color(255, 0, 0)),
+      rect(Point2D(300, 100), 150, 100, Color(0, 255, 0), Color(0, 0, 0)),
+      circle(Point2D(400, 400), 80, Color(0, 0, 255)),
+      triangle(Point2D(600, 300), 60, Color(255, 255, 0)),
+      //image("assets/test_static.png", Point2D(400, 300)),
+      anim("assets/samurai/RUN.png", 96, 96, 16, 0.05f, Point2D(200, 200), 4.0f)
+{}
 
 void Scene2::update(float dt, const Input& input) {
     timer += dt;
+    anim.update(dt);
 
-    // Наприклад, автоматичний перехід через 5 секунд
-    if (timer > 5.0f) {
-        finished = true;
+    float rectSpeed = 2.0f;
+    if (input.isKeyPressed(ALLEGRO_KEY_LEFT)) {
+        rect.pos.x -= rectSpeed;
+    }
+    if (input.isKeyPressed(ALLEGRO_KEY_RIGHT)) {
+        rect.pos.x += rectSpeed;
+    }
+    if (input.isKeyPressed(ALLEGRO_KEY_UP)) {
+        rect.pos.y -= rectSpeed;
+    }
+    if (input.isKeyPressed(ALLEGRO_KEY_DOWN)) {
+        rect.pos.y += rectSpeed;
     }
 
-    // Або: перехід по клавіші
-    ALLEGRO_KEYBOARD_STATE state;
-    al_get_keyboard_state(&state);
-    if (al_key_down(&state, ALLEGRO_KEY_ENTER)) {
-        finished = true;
+    circle.center.x = input.mouseX;
+    circle.center.y = input.mouseY;
+
+    if (input.isKeyJustPressed(ALLEGRO_KEY_F)) {
+        circle.toggleFill();
     }
+
+    if (input.isKeyJustPressed(ALLEGRO_KEY_T)) {
+        triangle.toggleFill();
+    }
+
+    if (input.isKeyPressed(ALLEGRO_KEY_ESCAPE)) {
+        timer = 999.0f;
+    }
+
+    triangle.update();
 }
 
 void Scene2::draw(Renderer& r) {
-    r.clear(Color(30, 30, 60));
-
-    // Малюємо простий текст
-    ALLEGRO_FONT* font = al_create_builtin_font();
-    al_draw_text(font, al_map_rgb(255, 255, 255), 400, 300, ALLEGRO_ALIGN_CENTER, "Це сцена 2. Натисни Enter...");
-    al_destroy_font(font);
+    line.draw(r);
+    rect.draw(r);
+    circle.draw(r);
+    triangle.draw(r);
+    //image.draw(r);
+    anim.draw(r);
 }
 
 bool Scene2::isFinished() const {
-    return finished;
+    return timer > 5.0f;
 }
 
 Scene* Scene2::nextScene() const {
-    return new Scene1(); // або new Scene3();
+    return new Scene1();
 }
